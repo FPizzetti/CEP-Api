0b6557fcf83ee9a8e7c28d9a7aaf65520a11bc1e
FPizzetti <felipe_pizzetti@hotmail.com>
2015-10-07 22:16:37 -0300

    removed unused files

--- DbText/src/dbText/DbTextInterface.java
+++
@@ -1,9 +0,0 @@
-package dbText;
-
-public interface DbTextInterface
-{
-	boolean equals(Object object);
-
-	Object createObject(Object... objects);
-
-}
--- Sphinx4Project/src/TesteReconhecedor.java
+++
@@ -1,9 +0,0 @@
-public class TesteReconhecedor {
-	public static void main(String[] args) {
-		Reconhecedor reconhecedor = new Reconhecedor();
-
-		String result = reconhecedor.listenToIt();
-
-		System.out.println(result);
-	}
-}
